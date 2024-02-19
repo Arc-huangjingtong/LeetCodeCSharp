4e9702885d20605f595e521f08e5f921f50760fe
Arc <87562566+Arc-huangjingtong@users.noreply.github.com>
2024-02-20 00:28:50 +0800

    Initial commit

---
+++ LeetCodeCSharp/LeetCodeClass.cs
@@ -0,0 +1,38 @@
+﻿namespace LeetCodeCSharp;
+
+/// Definition for singly-linked list.
+public class ListNode(int val = 0, ListNode next = null)
+{
+    public int      val  = val;
+    public ListNode next = next;
+}
+
+
+/// Definition for a binary tree node.
+public class TreeNode(int val = 0, TreeNode left = null, TreeNode right = null)
+{
+    public int      val   = val;
+    public TreeNode left  = left;
+    public TreeNode right = right;
+}
+
+
+/// Definition for a Node.
+public class Node
+{
+    public int         val;
+    public IList<Node> children;
+
+    public Node() { }
+
+    public Node(int _val)
+    {
+        val = _val;
+    }
+
+    public Node(int _val, IList<Node> _children)
+    {
+        val      = _val;
+        children = _children;
+    }
+}
---
+++ LeetCodeCSharp/UnitTest1.cs
@@ -0,0 +1,13 @@
+namespace LeetCodeCSharp;
+
+[TestClass]
+public class UnitTest1
+{
+    [TestMethod]
+    public void TestMethod1() { }
+
+    public TreeNode BuildTree(int[] preorder, int[] inorder)
+    {
+        return null;
+    }
+}
