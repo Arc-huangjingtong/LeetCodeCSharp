371a827e7fd725d5f91e9df48aa6e8550872c5ab
Arc <87562566+Arc-huangjingtong@users.noreply.github.com>
2024-02-21 22:29:54 +0800

    Update

--- LeetCodeCSharp/UnitTest_1.cs
+++ LeetCodeCSharp/UnitTest_1.cs
@@ -17,4 +17,34 @@ public partial class UnitTest
 
         Assert.Pass();
     }
+
+
+    public TreeNode BuildTree(int[] inorder, int[] postorder)
+    {
+        var inorderMap = new Dictionary<int, int>();
+
+        for (var i = 0 ; i < inorder.Length ; i++)
+        {
+            inorderMap[inorder[i]] = i;
+        }
+
+        return BuildTree(0, inorder.Length - 1, 0, postorder.Length - 1);
+
+        TreeNode BuildTree(int inStart, int inEnd, int postStart, int postEnd)
+        {
+            if (inStart > inEnd || postStart > postEnd)
+            {
+                return null;
+            }
+
+            var root  = new TreeNode(postorder[postEnd]);
+            var index = inorderMap[root.val];
+
+            root.left = BuildTree(inStart, index - 1, postStart, postStart + index - inStart - 1);
+
+            root.right = BuildTree(index + 1, inEnd, postStart + index - inStart, postEnd - 1);
+
+            return root;
+        }
+    }
 }
