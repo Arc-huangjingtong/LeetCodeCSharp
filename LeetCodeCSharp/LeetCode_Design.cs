e57ef51a8b03b74ace09f313877dd59024c8d051
Arc <87562566+Arc-huangjingtong@users.noreply.github.com>
2024-04-16 19:05:25 +0800

    Update

--- LeetCodeCSharp/LeetCode_Design.cs
+++ LeetCodeCSharp/LeetCode_Design.cs
@@ -1,160 +1,156 @@
-﻿///////////////////////////////////////////////////////设计相关题型///////////////////////////////////////////////////////
+﻿namespace LeetCodeCSharp;
+///////////////////////////////////////////////////////设计相关题型///////////////////////////////////////////////////////
 
 
-namespace LeetCodeCSharp
+/// <summary> 设计哈希集合 </summary>
+public class Solution_705
 {
-
-    /// <summary> 设计哈希集合 </summary>
-    public class Solution_705
+    public class MyHashSet
     {
-        public class MyHashSet
-        {
-            private readonly bool[] _hashSet = new bool[1000001];
+        private readonly bool[] _hashSet = new bool[1000001];
 
 
-            public void Add(int key) => _hashSet[key] = true;
+        public void Add(int key) => _hashSet[key] = true;
 
-            public void Remove(int key) => _hashSet[key] = false;
+        public void Remove(int key) => _hashSet[key] = false;
 
-            public bool Contains(int key) => _hashSet[key];
-        }
-
-
-        public class MyHashSet2
-        {
-            public readonly IList<int>[] set = new IList<int>[BASE];
-
-            private const int BASE = 769;
-
-            public MyHashSet2()
-            {
-                for (var i = 0 ; i < BASE ; ++i)
-                {
-                    set[i] = [];
-                }
-            }
-
-
-
-            // 类似于桶排序,将key分流到不同的list中
-            public void Add(int key)
-            {
-                if (Contains(key)) return;
-
-                set[Hash(key)].Add(key);
-            }
-
-            public void Remove(int key) => set[Hash(key)].Remove(key);
-
-            // 最终利用分流后的list循环遍历判断是否存在
-            public bool Contains(int key) => set[Hash(key)].Contains(key);
-
-            // 通过取模运算,将key映射到哈希表中的索引
-            private static int Hash(int key) => key % BASE;
-        }
+        public bool Contains(int key) => _hashSet[key];
     }
 
 
-    /// <summary> 设计哈希映射 </summary>
-    public class Solution_706
+    public class MyHashSet2
     {
-        //0 <= key, value <= 10^6
-        public class MyHashMap2
+        public readonly IList<int>[] set = new IList<int>[BASE];
+
+        private const int BASE = 769;
+
+        public MyHashSet2()
         {
-            private readonly int[] _hashMap = new int[1000001];
-
-            public MyHashMap2() => Array.Fill(_hashMap, -1);
-
-
-            public void Put(int    key, int value) => _hashMap[key] = value;
-            public int  Get(int    key) => _hashMap[key];
-            public void Remove(int key) => _hashMap[key] = -1;
+            for (var i = 0 ; i < BASE ; ++i)
+            {
+                set[i] = [];
+            }
         }
 
 
-        public class MyHashMap
+
+        // 类似于桶排序,将key分流到不同的list中
+        public void Add(int key)
         {
-            public readonly Node[] bucket;
-            public const    int    hashCode = 128;
+            if (Contains(key)) return;
 
-            public MyHashMap()
-            {
-                bucket = new Node[128];
-                for (var i = 0 ; i < 128 ; i++)
-                {
-                    bucket[i] = new Node(-1, -1);
-                }
-            }
-
-            public void Put(int key, int value)
-            {
-                var index = key % hashCode;
-                var p     = bucket[index];
-                while (p.next != null)
-                {
-                    if (p.key == key)
-                    {
-                        p.value = value;
-                        return;
-                    }
-
-                    p = p.next;
-                }
-
-                p.key   = key;
-                p.value = value;
-                p.next  = new Node(-1, -1);
-            }
-
-            public int Get(int key)
-            {
-                var index = key % hashCode;
-                var p     = bucket[index];
-                while (p.next != null)
-                {
-                    if (p.key == key)
-                    {
-                        return p.value;
-                    }
-
-                    p = p.next;
-                }
-
-                return -1;
-            }
-
-            public void Remove(int key)
-            {
-                var index = key % hashCode;
-                var p     = bucket[index];
-                while (p.next != null)
-                {
-                    if (p.key == key)
-                    {
-                        bucket[index] = p.next;
-                        return;
-                    }
-
-                    if (p.next.key == key)
-                    {
-                        p.next = p.next.next;
-                        return;
-                    }
-
-                    p = p.next;
-                }
-
-                return;
-            }
-
-
-            public class Node(int k, int v)
-            {
-                public int  key   = k;
-                public int  value = v;
-                public Node next;
-            }
+            set[Hash(key)].Add(key);
         }
+
+        public void Remove(int key) => set[Hash(key)].Remove(key);
+
+        // 最终利用分流后的list循环遍历判断是否存在
+        public bool Contains(int key) => set[Hash(key)].Contains(key);
+
+        // 通过取模运算,将key映射到哈希表中的索引
+        private static int Hash(int key) => key % BASE;
+    }
+}
+
+
+/// <summary> 设计哈希映射 </summary>
+public class Solution_706
+{
+    //0 <= key, value <= 10^6
+    public class MyHashMap2
+    {
+        private readonly int[] _hashMap = new int[1000001];
+
+        public MyHashMap2() => Array.Fill(_hashMap, -1);
+
+
+        public void Put(int    key, int value) => _hashMap[key] = value;
+        public int  Get(int    key) => _hashMap[key];
+        public void Remove(int key) => _hashMap[key] = -1;
     }
 
+
+    public class MyHashMap
+    {
+        public readonly Node[] bucket;
+        public const    int    hashCode = 128;
+
+        public MyHashMap()
+        {
+            bucket = new Node[128];
+            for (var i = 0 ; i < 128 ; i++)
+            {
+                bucket[i] = new Node(-1, -1);
+            }
+        }
+
+        public void Put(int key, int value)
+        {
+            var index = key % hashCode;
+            var p     = bucket[index];
+            while (p.next != null)
+            {
+                if (p.key == key)
+                {
+                    p.value = value;
+                    return;
+                }
+
+                p = p.next;
+            }
+
+            p.key   = key;
+            p.value = value;
+            p.next  = new Node(-1, -1);
+        }
+
+        public int Get(int key)
+        {
+            var index = key % hashCode;
+            var p     = bucket[index];
+            while (p.next != null)
+            {
+                if (p.key == key)
+                {
+                    return p.value;
+                }
+
+                p = p.next;
+            }
+
+            return -1;
+        }
+
+        public void Remove(int key)
+        {
+            var index = key % hashCode;
+            var p     = bucket[index];
+            while (p.next != null)
+            {
+                if (p.key == key)
+                {
+                    bucket[index] = p.next;
+                    return;
+                }
+
+                if (p.next.key == key)
+                {
+                    p.next = p.next.next;
+                    return;
+                }
+
+                p = p.next;
+            }
+
+            return;
+        }
+
+
+        public class Node(int k, int v)
+        {
+            public int  key   = k;
+            public int  value = v;
+            public Node next;
+        }
+    }
 }
---
+++ LeetCodeCSharp/LeetCode_Graph.cs
@@ -0,0 +1,185 @@
+﻿namespace LeetCodeCSharp;
+///////////////////////////////////////////////////////图论相关题型///////////////////////////////////////////////////////
+
+
+public class Solution_924
+{
+    public int MinMalwareSpread(int[][] graph, int[] initial)
+    {
+        //1.分框操作:一共有N个节点,每个节点都有一个集合,集合中存放的是与该节点相连的节点
+        List<HashSet<int>> sets = [];
+
+        for (var i = 0 ; i < graph.Length ; i++)
+        {
+            sets.Add([i]);
+        }
+
+        //2.遍历所有的节点,将相连的节点的集合合并到一起
+        for (var i = 0 ; i < graph.Length ; i++)
+        {
+            for (var j = 0 ; j < graph.Length ; j++)
+            {
+                if (graph[i][j] == 1)
+                {
+                    sets[i].UnionWith(sets[j]);
+                    sets[j] = sets[i];
+                }
+            }
+        }
+
+        //3.删除重复的集合
+        sets = sets.Distinct().ToList();
+        //4.统计initial数组中的节点所在多少个框中
+        var counts = new int[sets.Count];
+
+        foreach (var node in initial)
+        {
+            foreach (var set in sets)
+            {
+                if (set.Contains(node))
+                {
+                    counts[sets.IndexOf(set)]++;
+                    break;
+                }
+            }
+        }
+
+        //5.找到initial数组中能拯救最多的节点
+        var saves = new int[sets.Count];
+
+
+        for (var i = 0 ; i < saves.Length ; i++)
+        {
+            saves[i] = counts[i] == 1 ? sets[i].Count : 0;
+        }
+
+
+        if (saves.All(x => x == 0) || saves.Count(x => x == saves.Max()) == saves.Length)
+        {
+            return initial.Min();
+        }
+
+        var saveList = new List<int>(saves);
+        saveList.RemoveAll(x => x == 0);
+
+        if (saveList.Count != 1 && saveList.Count == saveList.Count(x => x == saveList.Max()))
+        {
+            return initial.Min();
+        }
+
+
+        var maxSaveSet = sets[saves.ToList().IndexOf(saves.Max())];
+
+        //6.找到initial数组中能拯救最多的节点中的最小节点
+
+        maxSaveSet.IntersectWith(initial);
+
+        return maxSaveSet.Min();
+    }
+
+
+
+    //推荐:直接枚举所有点,找到能拯救最多的点
+    public int MinMalwareSpread2(int[][] graph, int[] initial)
+    {
+        var len      = graph.Length;
+        var ids      = new int[len];
+        var idToSize = new Dictionary<int, int>();
+        var id       = 0;
+
+        for (var i = 0 ; i < len ; ++i)
+        {
+            if (ids[i] != 0) continue;
+
+            id++;
+            var size  = 1;
+            var queue = new Queue<int>();
+            queue.Enqueue(i);
+            ids[i] = id;
+
+            while (queue.Count > 0)
+            {
+                var u = queue.Dequeue();
+                for (var v = 0 ; v < len ; ++v)
+                {
+                    if (ids[v] == 0 && graph[u][v] == 1)
+                    {
+                        ++size;
+                        queue.Enqueue(v);
+                        ids[v] = id;
+                    }
+                }
+            }
+
+            idToSize.Add(id, size);
+        }
+
+        var idToInitials = new Dictionary<int, int>();
+
+        foreach (var u in initial)
+        {
+            idToInitials.TryAdd(ids[u], 0);
+            idToInitials[ids[u]]++;
+        }
+
+        int ans = len + 1, ansRemoved = 0;
+
+        foreach (var u in initial)
+        {
+            var removed = (idToInitials[ids[u]] == 1 ? idToSize[ids[u]] : 0);
+            if (removed > ansRemoved || (removed == ansRemoved && u < ans))
+            {
+                ans        = u;
+                ansRemoved = removed;
+            }
+        }
+
+        return ans;
+    }
+
+
+
+    [Test]
+    public void Test()
+    {
+        int[][] graph = [[1, 1, 0], [1, 1, 0], [0, 0, 1]];
+        // [
+        //   // 0  1  2  3  4  5  6  7  8  9  10
+        //     [1, 0, 0, 0, 1, 0, 0, 0, 0, 0, 1], // 0
+        //     [0, 1, 0, 1, 0, 0, 0, 0, 0, 0, 0]  // 1
+        //   , [0, 0, 1, 0, 0, 0, 0, 1, 0, 0, 0]  // 2
+        //   , [0, 1, 0, 1, 0, 1, 0, 0, 0, 0, 0]  // 3
+        //   , [1, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0]  // 4
+        //   , [0, 0, 0, 1, 0, 1, 0, 0, 1, 1, 0]  // 5
+        //   , [0, 0, 0, 0, 0, 0, 1, 1, 0, 0, 0]  // 6
+        //   , [0, 0, 1, 0, 0, 0, 1, 1, 0, 0, 0]  // 7
+        //   , [0, 0, 0, 0, 0, 1, 0, 0, 1, 0, 0]  // 8
+        //   , [0, 0, 0, 0, 0, 1, 0, 0, 0, 1, 0]  // 9
+        //   , [1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1]  //10
+        // ];
+
+
+        //[   0,1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17]
+        //0  [1,0,0,0,0,0,0,0,0,0, 0, 0, 1, 0, 0, 0, 0, 1],
+        //1  [0,1,0,0,0,0,0,1,0,0, 0, 0, 0, 0, 1, 0, 0, 0],
+        //2  [0,0,1,0,0,1,0,0,0,0, 0, 0, 0, 0, 0, 0, 0, 0],
+        //3  [0,0,0,1,0,0,0,1,0,0, 0, 1, 0, 1, 1, 0, 0, 0],
+        //4  [0,0,0,0,1,0,0,0,0,0, 0, 0, 0, 0, 0, 1, 0, 1],
+        //5  [0,0,1,0,0,1,0,1,0,0, 0, 0, 0, 1, 0, 0, 0, 0],
+        //6  [0,0,0,0,0,0,1,0,0,0, 0, 0, 1, 0, 0, 1, 1, 0],
+        //7  [0,1,0,1,0,1,0,1,0,0, 0, 0, 0, 0, 0, 0, 0, 0],
+        //8  [0,0,0,0,0,0,0,0,1,0, 0, 0, 1, 0, 0, 0, 0, 0],
+        //9  [0,0,0,0,0,0,0,0,0,1, 0, 0, 0, 0, 1, 0, 0, 0],
+        //10 [0,0,0,0,0,0,0,0,0,0, 1, 0, 1, 0, 0, 0, 0, 0],
+        //11 [0,0,0,1,0,0,0,0,0,0, 0, 1, 0, 0, 0, 0, 0, 0],
+        //12 [1,0,0,0,0,0,1,0,1,0, 1, 0, 1, 0, 0, 0, 0, 1],
+        //13 [0,0,0,1,0,1,0,0,0,0, 0, 0, 0, 1, 0, 0, 0, 0],
+        //14 [0,1,0,1,0,0,0,0,0,1, 0, 0, 0, 0, 1, 0, 0, 0],
+        //15 [0,0,0,0,1,0,1,0,0,0, 0, 0, 0, 0, 0, 1, 0, 0],
+        //16 [0,0,0,0,0,0,1,0,0,0, 0, 0, 0, 0, 0, 0, 1, 0],
+        //17 [1,0,0,0,1,0,0,0,0,0, 0, 0, 1, 0, 0, 0, 0, 1]]
+        int[] initial = [0, 1, 2];
+
+        Console.WriteLine(MinMalwareSpread(graph, initial));
+    }
+}
