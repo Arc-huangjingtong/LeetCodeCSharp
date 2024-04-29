799bfd0f0227f9f579f63633e9f009a53e7b31c9
Arc <87562566+Arc-huangjingtong@users.noreply.github.com>
2024-04-29 19:03:27 +0800

    UPdate

---
+++ LeetCodeCSharp/LeetCode_Math.cs
@@ -0,0 +1,25 @@
+﻿namespace LeetCodeCSharp;
+///////////////////////////////////////////////////////数学相关题型///////////////////////////////////////////////////////
+
+
+///<summary> 1518. 换水问题 </summary>
+public class Solution_1518
+{
+    [TestCase(9,  3, ExpectedResult = 13)]
+    [TestCase(15, 4, ExpectedResult = 19)]
+    public int NumWaterBottles(int numBottles, int numExchange)
+    {
+        if (numBottles >= numExchange)
+        {
+            var extra = (numBottles - numExchange) / (numExchange - 1) + 1;
+            return extra + numBottles;
+        }
+
+        return numBottles;
+    }
+
+    //换水问题核心思路: 一次交换,n个空瓶子可以换1瓶水,实际上,因为一瓶水本身就是一个空瓶子,所以n-1个空瓶子可以换1瓶水
+    //但是,如果空瓶子的数量小于n,那么就无法换水了,所以这个时候,我们就可以直接返回空瓶子的数量
+    //所以只需要看倒数第二次换水的时候,剩下的空瓶子数量是否大于等于n,如果大于等于n,那么就可以继续换水,否则就直接返回
+    //公式: b−n(e−1)≥e
+}
---
+++ LeetCodeCSharp/LeetCode_Matrix.cs
@@ -0,0 +1,50 @@
+﻿namespace LeetCodeCSharp;
+///////////////////////////////////////////////////////矩阵相关题型///////////////////////////////////////////////////////
+
+
+///<summary> 1329. 将矩阵按对角线排序 </summary>
+public class Solution_1329
+{
+    
+    public int[][] DiagonalSort(int[][] mat)
+    {
+        var width  = mat.Length;
+        var length = mat[0].Length;
+
+
+        for (int i = 0 ; i < width ; i++)
+        {
+            for (var j = 0 ; j < length-1 ; j++)
+            {
+                
+            }
+        }
+
+
+        return mat;
+    }
+
+    [Test]
+    public void Test()
+    {
+        int[][] mat =
+        [
+            [11, 25, 66,  1, 69, 7],  // 
+            [23, 55, 17, 45, 15, 52] //
+          , [75, 31, 36, 44, 58, 8]  //
+          , [22, 27, 33, 25, 68, 4]  //
+          , [84, 28, 14, 11,  5, 50]
+        ];
+        //[[5, 17, 4, 1,52, 7],
+        // [11,11,25,45,8 ,69],
+        // [14,23,25,44,58,15],
+        // [22,27,31,36,50,66],
+        // [84,28,75,33,55,68]]
+
+        var result = DiagonalSort(mat);
+        foreach (var row in result)
+        {
+            Console.WriteLine(string.Join(",", row));
+        }
+    }
+}
