1fc64d16644110454e01052b55add78a9b88e868
Arc <87562566+Arc-huangjingtong@users.noreply.github.com>
2024-08-31 16:39:11 +0800

    Update Form_PrefixSum.cs

--- LeetCodeCSharp/Form_PrefixSum.cs
+++ LeetCodeCSharp/Form_PrefixSum.cs
@@ -1160,6 +1160,8 @@ public class Solution_1477
 
 
 /***************************************************  距离和  **********************************************************/
+// 距离和一般指的是两个数之间的距离和,这个距离和可以是绝对值,也可以是平方和,也可以是其他的,特征是需要能有条理的统计距离的正负
+// 注意：这类题目一般都比较大，尤其是对本身坐标相乘的时候，尽量使用long，防止溢出
 
 
 ///<summary> 1685. 有序数组中差绝对值之和 算术评级: 6 第 41 场双周赛Q2-1496 </summary>
@@ -1233,6 +1235,69 @@ public class Solution_1685
     //
     // 2 <= nums.length <= 10^5
     // 1 <= nums[i] <= nums[i + 1] <= 10^4
+}
+
+
+/// <summary> 2615. 等值距离和 算术评级: 6 第 340 场周赛Q2-1793 </summary>
+public class Solution_2615
+{
+    [TestCase(new[] { 1, 3, 1, 1, 2 }, ExpectedResult = new long[] { 5, 0, 3, 4, 0 })]
+    public long[] Distance(int[] nums)
+    {
+        var result = new long[nums.Length];
+        var dict   = new Dictionary<int, (long, long)>();
+
+        for (int i = 0, len = nums.Length ; i < len ; i++)
+        {
+            dict.TryAdd(nums[i], (0, 0));
+            result[i]     = dict[nums[i]].Item1 * i - dict[nums[i]].Item2;
+            dict[nums[i]] = (dict[nums[i]].Item1 + 1, dict[nums[i]].Item2 + i);
+        }
+
+        dict.Clear();
+
+        for (int i = nums.Length - 1 ; i >= 0 ; i--)
+        {
+            dict.TryAdd(nums[i], (0, 0));
+            dict[nums[i]] =  (dict[nums[i]].Item1 + 1, dict[nums[i]].Item2 + i);
+            result[i]     += dict[nums[i]].Item2 - dict[nums[i]].Item1 * i;
+        }
+
+
+        return result;
+    }
+
+
+
+    // 给你一个下标从 0 开始的整数数组 nums 。
+    // 现有一个长度等于 nums.length 的数组 arr 。
+    // 对于满足 nums[j] == nums[i] 且 j != i 的所有 j ，arr[i] 等于所有 |i - j| 之和。如果不存在这样的 j ，则令 arr[i] 等于 0 。
+    //
+    // 返回数组 arr 。
+    //
+    //
+    //
+    // 示例 1：
+    //
+    // 输入：nums = [1,3,1,1,2]
+    // 输出：[5,0,3,4,0]
+    // 解释：
+    // i = 0 ，nums[0] == nums[2] 且 nums[0] == nums[3] 。因此，arr[0] = |0 - 2| + |0 - 3| = 5 。 
+    // i = 1 ，arr[1] = 0 因为不存在值等于 3 的其他下标。
+    // i = 2 ，nums[2] == nums[0] 且 nums[2] == nums[3] 。因此，arr[2] = |2 - 0| + |2 - 3| = 3 。
+    // i = 3 ，nums[3] == nums[0] 且 nums[3] == nums[2] 。因此，arr[3] = |3 - 0| + |3 - 2| = 4 。 
+    // i = 4 ，arr[4] = 0 因为不存在值等于 2 的其他下标。
+    // 示例 2：
+    //
+    // 输入：nums = [0,5,3]
+    // 输出：[0,0,0]
+    // 解释：因为 nums 中的元素互不相同，对于所有 i ，都有 arr[i] = 0 。
+    //
+    //
+    // 提示：
+    //
+    // 1 <= nums.length <= 105
+    // 0 <= nums[i] <= 109
 }
 
 
