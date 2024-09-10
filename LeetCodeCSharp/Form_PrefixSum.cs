84bb0bbcb5c0c4b49bedbc5695282b8dabd29dba
Arc <87562566+Arc-huangjingtong@users.noreply.github.com>
2024-09-10 10:41:24 +0800

    Update Form_PrefixSum.cs

--- LeetCodeCSharp/Form_PrefixSum.cs
+++ LeetCodeCSharp/Form_PrefixSum.cs
@@ -1884,6 +1884,7 @@ public class Solution_2860
 
 /*****************************************************  一维差分  *******************************************************/
 // 差分思想主要是构造一个差分数组,负责记录每个位置的变化量,然后通过差分数组来计算原数组的变化
+// 目前看来,主要是用来解决一些区间覆盖问题
 
 
 /// <summary> 2848. 与车相交的点 算术评级: 2 第 362 场周赛Q1-1230 </summary>
@@ -1942,4 +1943,66 @@ public class Solution_2848
     // 1 <= nums.length <= 100
     // nums[i].length == 2
     // 1 <= starti <= endi <= 100
+}
+
+
+/// <summary> 1893. 检查是否区域内所有整数都被覆盖 算术评级: 2 第 54 场双周赛Q1-1307 </summary>
+public class Solution_1893
+{
+    public bool IsCovered(int[][] ranges, int left, int right)
+    {
+        Span<int> diff = stackalloc int[52];
+
+        foreach (var range in ranges)
+        {
+            diff[range[0]]++;
+            diff[range[1] + 1]--;
+        }
+
+        int sum = 0;
+
+        for (var i = 1 ; i <= 50 ; i++)
+        {
+            sum += diff[i];
+            if (i >= left && i <= right && sum <= 0)
+            {
+                return false;
+            }
+        }
+
+        return true;
+    }
+
+    // 
+    // 相关标签
+    //     相关企业
+    // 提示
+    //     给你一个二维整数数组 ranges 和两个整数 left 和 right 。每个 ranges[i] = [starti, endi] 表示一个从 starti 到 endi 的 闭区间 。
+    //
+    // 如果闭区间 [left, right] 内每个整数都被 ranges 中 至少一个 区间覆盖，那么请你返回 true ，否则返回 false 。
+    //
+    // 已知区间 ranges[i] = [starti, endi] ，如果整数 x 满足 starti <= x <= endi ，那么我们称整数x 被覆盖了。
+    //
+    //
+    //
+    // 示例 1：
+    //
+    // 输入：ranges = [[1,2],[3,4],[5,6]], left = 2, right = 5
+    // 输出：true
+    // 解释：2 到 5 的每个整数都被覆盖了：
+    // - 2 被第一个区间覆盖。
+    // - 3 和 4 被第二个区间覆盖。
+    // - 5 被第三个区间覆盖。
+    // 示例 2：
+    //
+    // 输入：ranges = [[1,10],[10,20]], left = 21, right = 21
+    // 输出：false
+    // 解释：21 没有被任何一个区间覆盖。
+    //
+    //
+    // 提示：
+    //
+    // 1 <= ranges.length <= 50
+    // 1 <= starti <= endi <= 50
+    // 1 <= left <= right <= 50
 }
