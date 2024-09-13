b4aa75feacac5f4f4ac9e397ce621c8e79156fdb
Arc <87562566+Arc-huangjingtong@users.noreply.github.com>
2024-09-13 19:03:18 +0800

    Update Form_PrefixSum.cs

--- LeetCodeCSharp/Form_PrefixSum.cs
+++ LeetCodeCSharp/Form_PrefixSum.cs
@@ -1885,6 +1885,8 @@ public class Solution_2860
 /*****************************************************  一维差分  *******************************************************/
 // 差分思想主要是构造一个差分数组,负责记录每个位置的变化量,然后通过差分数组来计算原数组的变化
 // 目前看来,主要是用来解决一些区间覆盖问题
+// 注意点就是区间的大小可以用排序字典来优化
+// 边界问题也需要注意,例如2406题,尾端点需要加一
 
 
 /// <summary> 2848. 与车相交的点 算术评级: 2 第 362 场周赛Q1-1230 </summary>
@@ -2197,7 +2199,6 @@ public class Solution_732
 
             return ans;
         }
-        
     }
 
 
@@ -2239,4 +2240,112 @@ public class Solution_732
     // 提示:
     // 0 <= startTime < endTime <= 10^9
     // 每个测试用例，调用 book 函数最多不超过 400次
+}
+
+
+/// <summary> 2406. 将区间分为最少组数 算术评级: 5 第 310 场周赛 Q3-1713 </summary>
+public class Solution_2406
+{
+    public int MinGroups(int[][] intervals)
+    {
+        var diff = new SortedDictionary<int, int>();
+        foreach (var interval in intervals)
+        {
+            var index1 = interval[0];
+            var index2 = interval[1] + 1;
+
+            diff.TryAdd(index1, 0);
+            diff.TryAdd(index2, 0);
+
+
+            diff[index1]++;
+            diff[index2]--;
+        }
+
+        var sum = 0;
+        var ans = 0;
+
+
+        foreach (var pair in diff)
+        {
+            sum += pair.Value;
+            ans =  Math.Max(ans, sum);
+        }
+
+        return ans;
+    }
+
+
+    // 给你一个二维整数数组 intervals ，其中 intervals[i] = [lefti, righti] 表示 闭 区间 [lefti, righti] 。
+    //
+    // 你需要将 intervals 划分为一个或者多个区间 组 ，每个区间 只 属于一个组，且同一个组中任意两个区间 不相交 。
+    //
+    // 请你返回 最少 需要划分成多少个组。
+    //
+    // 如果两个区间覆盖的范围有重叠（即至少有一个公共数字），那么我们称这两个区间是 相交 的。比方说区间 [1, 5] 和 [5, 8] 相交。
+    //
+    //
+    //
+    // 示例 1：
+    //
+    // 输入：intervals = [[5,10],[6,8],[1,5],[2,3],[1,10]] 
+    // 输出：3
+    // 解释：我们可以将区间划分为如下的区间组：
+    // - 第 1 组：[1, 5] ，[6, 8] 。
+    // - 第 2 组：[2, 3] ，[5, 10] 。
+    // - 第 3 组：[1, 10] 。
+    // 可以证明无法将区间划分为少于 3 个组。
+    // 示例 2：
+    //
+    // 输入：intervals = [[1,3],[5,6],[8,10],[11,13]]
+    // 输出：1
+    // 解释：所有区间互不相交，所以我们可以把它们全部放在一个组内。
+    //
+    //
+    // 提示：
+    //
+    // 1 <= intervals.length <= 10^5
+    // intervals[i].length == 2
+    // 1 <= lefti <= righti <= 10^6
+}
+
+
+/// <summary> 2381. 字母移位 II 算术评级: 5 第 85 场双周赛Q3-1793 </summary>
+public class Solution_2381
+{
+    public string ShiftingLetters(string s, int[][] shifts)
+    {
+        return default;
+    }
+
+    // 给你一个小写英文字母组成的字符串 s 和一个二维整数数组 shifts ，其中 shifts[i] = [starti, endi, directioni] 。对于每个 i ，将 s 中从下标 starti 到下标 endi （两者都包含）所有字符都进行移位运算，如果 directioni = 1 将字符向后移位，如果 directioni = 0 将字符向前移位。
+    //
+    // 将一个字符 向后 移位的意思是将这个字符用字母表中 下一个 字母替换（字母表视为环绕的，所以 'z' 变成 'a'）。类似的，将一个字符 向前 移位的意思是将这个字符用字母表中 前一个 字母替换（字母表是环绕的，所以 'a' 变成 'z' ）。
+    //
+    // 请你返回对 s 进行所有移位操作以后得到的最终字符串。
+    //
+    //
+    //
+    // 示例 1：
+    //
+    // 输入：s = "abc", shifts = [[0,1,0],[1,2,1],[0,2,1]]
+    // 输出："ace"
+    // 解释：首先，将下标从 0 到 1 的字母向前移位，得到 s = "zac" 。
+    // 然后，将下标从 1 到 2 的字母向后移位，得到    s = "zbd" 。
+    // 最后，将下标从 0 到 2 的字符向后移位，得到    s = "ace" 。
+    // 示例 2:
+    //
+    // 输入：s = "dztz", shifts = [[0,0,0],[1,1,1]]
+    // 输出："catz"
+    // 解释：首先，将下标从 0 到 0 的字母向前移位，得到 s = "cztz" 。
+    // 最后，将下标从 1 到 1 的字符向后移位，得到    s = "catz" 。
+    //
+    //
+    // 提示：
+    //
+    // 1 <= s.length, shifts.length <= 5 * 104
+    // shifts[i].length == 3
+    // 0 <= starti <= endi < s.length
+    // 0 <= directioni <= 1
+    // s 只包含小写英文字母。
 }
