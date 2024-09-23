30cc381cf949d96877764da72e5424886c2f8b17
Arc <87562566+Arc-huangjingtong@users.noreply.github.com>
2024-09-23 19:08:09 +0800

    Update Form_Stack.cs

--- LeetCodeCSharp/Form_Stack.cs
+++ LeetCodeCSharp/Form_Stack.cs
@@ -378,3 +378,99 @@ public class Solution_3170
     // s 只含有小写英文字母和 '*' 字符。
     // 输入保证操作可以删除所有的 '*' 字符。
 }
+
+
+/// <summary> 155. 最小栈 算术评级: 4 中等 </summary>
+public class Solution_155
+{
+    public class MinStack
+    {
+        private Stack<int> _stack = [];
+
+        private int min = int.MaxValue;
+
+        public void Push(int val)
+        {
+            _stack.Push(val);
+
+            if (val < min)
+            {
+                min = val;
+            }
+        }
+
+        public void Pop()
+        {
+            _stack.Pop();
+
+            if (_stack.Count != 0)
+            {
+                min = _stack.Min();
+            }
+            else
+            {
+                min = int.MaxValue;
+            }
+        }
+
+        public int Top()
+        {
+            if (_stack.TryPeek(out var result))
+            {
+                return result;
+            }
+
+            return -1;
+        }
+
+        public int GetMin() => min;
+    }
+
+
+    /**
+     * Your MinStack object will be instantiated and called as such:
+     * MinStack obj = new MinStack();
+     * obj.Push(val);
+     * obj.Pop();
+     * int param_3 = obj.Top();
+     * int param_4 = obj.GetMin();
+     */
+
+
+    // 设计一个支持 push ，pop ，top 操作，并能在常数时间内检索到最小元素的栈。
+    //
+    // 实现 MinStack 类:
+    //
+    // MinStack() 初始化堆栈对象。
+    // void push(int val) 将元素val推入堆栈。
+    // void pop() 删除堆栈顶部的元素。
+    // int top() 获取堆栈顶部的元素。
+    // int getMin() 获取堆栈中的最小元素。
+    //  
+    //
+    // 示例 1:
+    //
+    // 输入：
+    // ["MinStack","push","push","push","getMin","pop","top","getMin"]
+    // [[],[-2],[0],[-3],[],[],[],[]]
+    //
+    // 输出：
+    // [null,null,null,null,-3,null,0,-2]
+    //
+    // 解释：
+    // MinStack minStack = new MinStack();
+    // minStack.push(-2);
+    // minStack.push(0);
+    // minStack.push(-3);
+    // minStack.getMin();   --> 返回 -3.
+    //                              minStack.pop();
+    // minStack.top();      --> 返回 0.
+    //     minStack.getMin();   --> 返回 -2.
+    //  
+    //
+    //     提示：
+    //
+    // -231 <= val <= 231 - 1
+    // pop、top 和 getMin 操作总是在 非空栈 上调用
+    //     push, pop, top, and getMin最多被调用 3 * 104 次
+}
